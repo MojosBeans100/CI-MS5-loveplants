6a956768e13c5496ec8053d973fd82f1f3bcd049
MojosBeans100 <lucy.colley@btinternet.com>
2022-04-01 07:51:10 +0000

    Add filter functionality to navbar

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.db.models import Q
 
 # import local
-from products.models import Product
+from products.models import Product, Category
 
 
 def all_products(request):
@@ -12,13 +12,20 @@ def all_products(request):
     """
 
     all_products = Product.objects.all()
+    search_query = None
+    category = None
 
-    if 'q' in request.GET:
-        search_query = request.GET['q']
-        # if not search_query:
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            category_id = Category.objects.get(name=categories)
+            all_products = all_products.filter(category=category_id.id)
+            print(all_products)
 
-        search_queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
-        all_products = all_products.filter(search_queries)
+        if 'q' in request.GET:
+            search_query = request.GET['q']
+            search_queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
+            all_products = all_products.filter(search_queries)
 
     context = {
         'all_products': all_products
