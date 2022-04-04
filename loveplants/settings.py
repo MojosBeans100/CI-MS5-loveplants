c52f63ab91f5cc29fdf7a4f2dbca73140da504e5
MojosBeans100 <lucy.colley@btinternet.com>
2022-04-04 07:21:14 +0000

    ADd account url to nav

--- loveplants/settings.py
+++ loveplants/settings.py
@@ -172,10 +172,10 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'loveplants@example.com'
 else:
-    EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLD = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
