ccb285146ce8866b847527ca0fde2695ceee740d
MojosBeans100 <lucy.colley@btinternet.com>
2022-04-12 14:56:37 +0000

    REgister order and orderlineitem in admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,38 @@
+
+# 3rd party imports
+
+# Django imports
 from django.contrib import admin
 
-# Register your models here.
+# Local imports
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Admin model to view all products associated with
+    an order
+    """
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin model to view all orders
+    """
+
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_ref', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    list_display = ('order_ref', 'date',
+                    'full_name', 'order_total',
+                    'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
