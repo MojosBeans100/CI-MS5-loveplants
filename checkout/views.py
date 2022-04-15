e7d266bc22605c528d3166a4ba02c6f7183f6366
MojosBeans100 <lucy.colley@btinternet.com>
2022-04-15 13:13:49 +0000

    Add to checkout view, save form, create order object

--- checkout/views.py
+++ checkout/views.py
@@ -8,7 +8,8 @@ from django.conf import settings
 
 # Local imports
 import os
-from .models import Order
+from .models import Order, OrderLineItem
+from products.models import Product
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
@@ -20,24 +21,64 @@ def view_checkout(request):
     """
     """
 
-    bag = request.session.get('bag', {})
+    if request.method == 'POST':
 
-    # if not bag:
-    #     messages.error(request, "There's nothing in your bag at the moment.")
-        #return redirect(reverse('products'))
+        bag = request.session.get('bag', {})
+        form_info = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_num': request.POST['phone_num'],
+            'street_address_1': request.POST['street_address_1'],
+            'street_address_2': request.POST['street_address_2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
 
-    current_bag = bag_contents(request)
-    current_total = current_bag['grand_total']
-    stripe_total = round(current_total*100)
-    stripe.api_key = stripe_sk
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency='gbp',
-    )
+        order_form = OrderForm(form_info)
 
-    print(intent)
+        if order_form.is_valid():
+            order_form.save()
+            order = order_form.save(commit=False)
 
-    order_form = OrderForm()
+            for item_id, item_data in bag.items():
+
+                product = Product.objects.get(id=item_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=item_data,
+                )
+                order_line_item.save()
+
+            return redirect(reverse(
+                            'checkout_success',
+                            args=[order.order_number]))
+
+        else:
+            messages.error(request, 'There is an error in the form')
+
+    else:
+
+        bag = request.session.get('bag', {})
+
+        # if not bag:
+        #     messages.error(request, "There's nothing in your bag at the moment.")
+            #return redirect(reverse('products'))
+
+        current_bag = bag_contents(request)
+        current_total = current_bag['grand_total']
+        stripe_total = round(current_total*100)
+        stripe.api_key = stripe_sk
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency='gbp',
+        )
+
+        print(intent)
+
+        order_form = OrderForm()
 
     context = {
         'order_form': order_form,
